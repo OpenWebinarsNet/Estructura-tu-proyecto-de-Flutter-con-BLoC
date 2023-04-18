5628589d08f898e6fa9ed4831dd10329b40f8dc3
David <davidrmsevilla@gmail.com>
2023-04-18 10:15:34 -0400

    first commit

---
+++ bloc-folder-structure/android/app/src/main/kotlin/com/example/bloc_folder_structure/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_folder_structure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bloc-state-management/theme_bloc_flutter/lib/theme/theme_state.dart
@@ -0,0 +1,29 @@
+part of 'theme_bloc.dart';
+
+enum AppTheme {
+  light,
+  dark,
+}
+
+class ThemeState {
+  final AppTheme appTheme;
+
+  const ThemeState({
+    this.appTheme = AppTheme.light,
+  });
+
+  factory ThemeState.initial() {
+    return const ThemeState();
+  }
+
+  @override
+  String toString() => 'ThemeState(appTheme: $appTheme)';
+
+  ThemeState copyWith({
+    AppTheme? appTheme,
+  }) {
+    return ThemeState(
+      appTheme: appTheme ?? this.appTheme,
+    );
+  }
+}
